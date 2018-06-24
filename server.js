92ffcebc1eee90b3ce6cfe808e18714501fbf0a3
EC2 Default User <ec2-user@ip-172-31-68-215.ec2.internal>
2018-06-24 04:25:44 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname,'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
